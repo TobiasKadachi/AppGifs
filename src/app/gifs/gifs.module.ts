613494f117c66137962cbff55c0923cb79b7e699
Victor Martin Arquelladas <vmn@stiga.es>
2024-10-01 17:28:01 +0200

    01/10

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchBoxComponent,
   ],
   imports: [
     CommonModule,
-    HomePageComponent
+    SearchBoxComponent,
+  ],
+  exports: [
+    HomePageComponent,
   ]
 })
 export class GifsModule { }
--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gifs-home-page',
-  templateUrl: './home-page.component.html',
+  templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
 
